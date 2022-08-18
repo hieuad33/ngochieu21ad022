4c8a424ff89d7c4a2926b9438f354b26bb808d54
hieuad33 <39626889+hieuad33@users.noreply.github.com>
2022-08-18 14:57:12 +0700

    Add files via upload

---
+++ baitapnhom4/bai6/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nhập và tính toán trên dãy số</title>
+	<meta charset="utf-8">
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+	<a class="btn btn-primary" href="ketqua61.php"> bai 6.1</a>
+	<a class="btn btn-danger" href="ketqua62.php"> bai 6.2</a>
+	<a class="btn btn-info" href="ketqua63.php">bài 6.3</a>
+</body>
+</html>
